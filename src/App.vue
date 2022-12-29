ae49633e9455c6fed7b6e6fb0f8bb326fa75589a
alex <2844520415@qq.com>
2022-12-29 13:43:38 +0800

    style: 抽离组件

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,9 @@
 <template>
-  <div class="logo">
-    <div class="logo-bg"></div>
-    <h1>
-      <a href="#">侗 语</a>
-    </h1>
-  </div>
+  <Logo />
+
 </template>
 
 <script setup>
+import Logo from './components/Logo.vue';
+
 </script>
-
-<style scoped>
-.logo {
-  position: relative;
-  height: 6em;
-  padding: 1.5em;
-
-}
-
-.logo-bg {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: linear-gradient(-45deg, #bd34fe 50%, #47caff 50%);
-  filter: blur(66px);
-
-}
-</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="logo">
+        <div class="logo-bg"></div>
+        <h1>
+            <a href="#">侗 语</a>
+        </h1>
+    </div>
+</template>
+  
+<script setup>
+</script>
+  
+<style scoped lang="less">
+.logo {
+    position: relative;
+    height: 6em;
+    padding: 1.5em;
+    cursor: pointer;
+
+    .logo-bg {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: linear-gradient(-45deg, #47caff 50%, #646cff 50%);
+        filter: blur(66px);
+        opacity: 0;
+        transition: .2s;
+
+        &:hover {
+            opacity: 1;
+        }
+    }
+
+}
+</style>
+  
